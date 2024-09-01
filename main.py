e567f79d53329cd488ac6a4c4b7c48f01e9af4b7
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-09-01 07:41:44 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+class Person():
+    def __init__(self,name,id):
+        n,s = name.split(' ')
+        self.name = n
+        self.surname = s
+        self.id = id
+    def __str__(self):
+        return f'id:{self.id}\nname : {self.name} {self.surname}'
+
+#person
+p1 = Person('Nuttaphon Popardit',1)
+# print(p1)
+
+class Student(Person):
+    def __init__(self,name,id,number):
+        self.__number = number
+        super().__init__(name,id)
+    def __str__(self):
+        return f'id:{self.id}\nname : {self.name} {self.surname}\nnumber:{self.__number}'
+
+s1 = Student('Nuttaphon Popardit',1,2)
+print(s1)
