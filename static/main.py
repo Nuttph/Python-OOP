3028101739450cfe36d1e80b78acb0ef7b9a7a1c
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-09-16 23:22:18 +0700

    abstract is ezzzzz

--- static/main.py
+++ static/main.py
@@ -1,17 +1,23 @@
-from abc import abstractmethod,ABC
+from abc import abstractmethod, ABC
 
 class Bank(ABC):
-    def __init__(self,money):
+    def __init__(self, money):
         self.money = money
+
     @staticmethod
-    def lfMoney(a,b):
+    def lfMoney(a, b):
         return a.money < b.money
-    
+
     @abstractmethod
     def abs(self):
         return True
-    
 
-A = Bank(10)
-B = Bank(5)
-print(Bank.lfMoney(A,B))
+class MyBank(Bank):
+    def abs(self):
+        return super().abs()  # สามารถ implement เมธอดนี้ตามความต้องการ
+
+if __name__ == "__main__":
+    A = MyBank(10)
+    B = MyBank(5)
+    print(Bank.lfMoney(A, B))  # ผลลัพธ์คือ True เพราะ 10 > 5
+    
